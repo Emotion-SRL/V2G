1385525695d7a1daf16e85bfe65155ccce62982f
Aldwyn47 <andrea.imparato89@gmail.com>
2024-02-26 17:34:04 +0100

    Implemented multiple threads

--- CAN_Zeka.py
+++ CAN_Zeka.py
@@ -1,9 +1,14 @@
 import os
+import threading
 import time
 
 import can
 
-from zeka_status import human_readable_feedback_2_status_response
+from zeka_control import assemble_main_control_command
+from zeka_status import (
+    feedback_2_status_request,
+    human_readable_feedback_2_status_response,
+)
 
 
 def can_init():
@@ -23,48 +28,8 @@ def can_fini():
     os.system('sudo ifconfig can1 down')
 
 
-def byte_to_temperature(high_byte, low_byte):
-    # Combinare i byte per ottenere il valore a 16 bit
-    value = (high_byte << 8) | low_byte
-
-    # Moltiplicare per 0.1 per ottenere la temperatura in gradi Celsius
-    temperature = round(value * 0.1, 1)
-
-    return temperature
-
-
-def bytes_to_temperature(feedback_array):
-    # Estrai i penultimi due byte
-    high_byte = feedback_array[-2]
-    low_byte = feedback_array[-1]
-
-    # Combinare i byte per ottenere il valore a 16 bit
-    value = (high_byte << 8) | low_byte
-
-    # Moltiplicare per 0.1 per ottenere la temperatura in gradi Celsius
-    temperature = round(value * 0.1, 2)
-
-    return temperature
-
-
-def boost_1Q_voltage_control(voltage, current):
-    # Calcola i byte per la tensione
-    voltage_value = int(voltage / 0.1)
-    voltage_high_byte = (voltage_value >> 8) & 0xFF
-    voltage_low_byte = voltage_value & 0xFF
-
-    # Calcola i byte per la corrente
-    current_value = int(current / 0.1)
-    current_high_byte = (current_value >> 8) & 0xFF
-    current_low_byte = current_value & 0xFF
-
-    # Crea il messaggio da inviare sul bus CAN
-    command = [0x83, voltage_high_byte, voltage_low_byte, current_high_byte, current_low_byte, 0x00, 0x00, 0x00]
-
-    return command
-
-
 can_init()
+
 
 psu_can_interface = 'socketcan'
 psu_can_channel = 'can0'
@@ -76,6 +41,9 @@ evi_baud_rate = 500000
 
 psu_bus = can.interface.Bus(channel=psu_can_channel, bustype=psu_can_interface, bitrate=psu_baud_rate)
 evi_bus = can.interface.Bus(channel=evi_can_channel, bustype=evi_can_interface, bitrate=evi_baud_rate)
+
+psu_lock = threading.Lock()
+evi_lock = threading.Lock()
 
 psu_master_node_id = 0x001  # Master ID con cui scrivere al device come Master della comunicazione CAN
 psu_slave_node_id = 0x004  # Slave ID con cui risonde il device
@@ -95,68 +63,60 @@ evi_status_packet_id = 0x4  # ID del pacchetto control = 1 status = 4
 evi_control_message_id = (evi_master_node_id << 8) | (evi_device_ID << 3) | evi_control_packet_id
 evi_status_message_id = (evi_master_node_id << 8) | (evi_device_ID << 3) | evi_status_packet_id
 
-# Status registers 0xA0 >>> 0xA4
-Main_Status_Request = [0xA0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-Feedback_1_Status_Request = [0xA1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-Feedback_2_Status_Request = [0xA2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-Error_Status_Request = [0xA3, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-IOs_Status_Request = [0xA4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+def thread_safe_BLG_CAN_request_response_cycle(request):
+    with psu_lock:
+        psu_bus.send(request)
+        response = psu_bus.recv(1)
+    return response
 
 
-# Control registers 0x80 >>> 0x90
-Main_Control_Command = [0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Buck_1Q_Voltage_Control_Reference_Command = [0x81, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Buck_1Q_Current_Control_Reference_Command = [0x82, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Boost_1Q_Voltage_Control_Reference_Command = [0x83, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Boost_1Q_Current_Control_Reference_Command = [0x84, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Buck_2Q_Voltage_Control_Reference_Command = [0x85, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Boost_2Q_Voltage_Control_Reference_Command = [0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Boost_A_current_B_Voltage_Control_Reference_Command = [0x8B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-Outputs_Control_Command = [0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #
-
-converter_setup = [0x80, 0x01, 0x81, 0x00, 0x02, 0x00, 0x00, 0x00]  # 60V low side buck
-
-# register = open("Registers.txt", "r")
-# print(register.readline())
+def initialize_BLG():
+    data_bytes = assemble_main_control_command(
+        precharge_delay=False,
+        reset_faults=False,
+        full_stop=False,
+        run_device=False,
+        set_device_mode="No mode selected"
+    )
+    message = can.Message(arbitration_id=psu_control_message_id, data=data_bytes, is_extended_id=False)
+    thread_safe_BLG_CAN_request_response_cycle(message)
 
 
-# Crea il messaggio di controllo
-psu_status_message = can.Message(arbitration_id=psu_status_message_id, data=Feedback_2_Status_Request, is_extended_id=False)
+def BLG_heartbeat(stop_psu_heartbeat, verbose=False):
+    print("BLG_heartbeat thread started")
+    message = can.Message(arbitration_id=psu_status_message_id, data=feedback_2_status_request, is_extended_id=False)
+    while not stop_psu_heartbeat.is_set():
+        response = thread_safe_BLG_CAN_request_response_cycle(message)
+        if verbose and response is not None:
+            print(human_readable_feedback_2_status_response(response))
+        time.sleep(1.4)
+    print("BLG_heartbeat thread stopped")
 
 
-# Invia il messaggio di controllo
-# psu_bus.send(psu_control_message)
-# response1 = psu_bus.recv(1.5)
-# print(response1)
-# psu_bus.send(psu_status_message)
-# response2 = psu_bus.recv(1.5)
-# print(response2)
-# time.sleep(0.5) # Attendi un breve periodo di tempo per assicurarti che il messaggio venga inviato
+def EVI_CAN_server(stop_evi_server):
+    print("EVI_CAN_server thread started")
+    while not stop_evi_server.is_set():
+        time.sleep(1.5)
+    print("EVI_CAN_server thread stopped")
+
+
 try:
-    voltage = 200
-    current = 3
-    voltage_message = boost_1Q_voltage_control(voltage, current)
-    psu_setup_message = can.Message(arbitration_id=psu_control_message_id, data=converter_setup, is_extended_id=False)
-    psu_voltage_control_message = can.Message(arbitration_id=psu_control_message_id, data=voltage_message, is_extended_id=False)
-    print(psu_setup_message)
-    psu_bus.send(psu_setup_message)
-    time.sleep(0.2)
-    psu_bus.send(psu_voltage_control_message)
-
-    while True:
-        psu_bus.send(psu_status_message)
-        response2 = psu_bus.recv(1.5)
-        # data = response2.data.hex()
-        # high_byte = int(data[-6:-4], 16)
-        # low_byte = int(data[-4:-2], 16)
-
-        # temperatura = byte_to_temperature(high_byte, low_byte)
-        # print("La temperatura è:", temperatura, "°C")
-        human_readable_feedback_2_status_response(response2.data)
-        # print(data,high_byte,low_byte)
-        time.sleep(0.5)
+    stop_psu_heartbeat = threading.Event()
+    stop_evi_server = threading.Event()
+    initialize_BLG()
+    blg_heartbeat_thread = threading.Thread(target=BLG_heartbeat, kwargs={'stop_psu_heartbeat': stop_psu_heartbeat, 'verbose': True})
+    evi_server_thread = threading.Thread(target=EVI_CAN_server, kwargs={'stop_evi_server': stop_evi_server})
+    blg_heartbeat_thread.start()
+    evi_server_thread.start()
+    keyboard_interrupt = threading.Event()
+    keyboard_interrupt.wait()
 except KeyboardInterrupt:
     # # Chiudi la connessione CAN
+    stop_psu_heartbeat.set()
+    stop_evi_server.set()
+    blg_heartbeat_thread.join()
+    evi_server_thread.join()
     psu_bus.shutdown()
     evi_bus.shutdown()
     can_fini()
